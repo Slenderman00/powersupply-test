2da60d8ef4ea88828c2b4a64c333ccd57fe01c81
Joar Heimonen <contact@joar.me>
2025-11-11 17:09:11 +0100

    feat: stuff

--- powersupply.py
+++ powersupply.py
@@ -1,49 +1,40 @@
 import tntapi
 from yangcli import yangcli
 
-
 class Power:
     def __init__(self, conns, yconns, node_name="power0"):
-        self.yconns = yconns
         self.conns = conns
+        self.yconns = yconns
         self.node_name = node_name
 
     def delete_powersupply(self):
-        yangcli(self.yconns[self.node_name], """delete /outputs""").xpath("./ok")
+        yangcli(self.yconns[self.node_name], "delete /outputs").xpath("./ok")
         tntapi.network_commit(self.conns)
 
-
-    def set_voltage(self, voltage):
+    def set_voltage(self, voltage, current_limit=0.5, output="out1"):
         yangcli(
             self.yconns[self.node_name],
-            """replace /outputs/output[name='out1'] -- voltage-level=%.9f current-limit=%.9f"""
-            % (voltage, 0.5),
+            "replace /outputs/output[name='%s'] -- voltage-level=%.9f current-limit=%.9f"
+            % (output, float(voltage), float(current_limit)),
         ).xpath("./ok")
         tntapi.network_commit(self.conns)
-
 
     def delete_voltage_dual(self):
         yangcli(self.yconns[self.node_name], "delete /outputs/output[name='out1']").xpath("./ok")
         yangcli(self.yconns[self.node_name], "delete /outputs/output[name='out2']").xpath("./ok")
-        self.tntapi.network_commit(self.conns)
+        tntapi.network_commit(self.conns)
 
+    def set_voltage_dual(self, voltage, current_limit=0.5):
+        for out in ("out1", "out2"):
+            yangcli(
+                self.yconns[self.node_name],
+                "replace /outputs/output[name='%s'] -- voltage-level=%.9f current-limit=%.9f"
+                % (out, float(voltage), float(current_limit)),
+            ).xpath("./ok")
+        tntapi.network_commit(self.conns)
 
-    def set_voltage_dual(self, voltage):
-        yangcli(
-            self.conns[self.node_name],
-            """replace /outputs/output[name='out1'] -- voltage-level=%.9f current-limit=%.9f"""
-            % (voltage, 0.5),
-        ).xpath("./ok")
-        yangcli(
-            self.conns[self.node_name],
-            """replace /outputs/output[name='out2'] -- voltage-level=%.9f current-limit=%.9f"""
-            % (voltage, 0.5),
-        ).xpath("./ok")
-        self.tntapi.network_commit(self.conns)
-
-
-    def voltage_sweep(self, callback, min=10, max=48, step=1, node_name="power0"):
-        for voltage in range(min, max, step):
-            print(f"# input voltage {voltage} volts")
-            set_voltage(voltage)
-            callback(voltage)
+    def voltage_sweep(self, callback, vmin=10, vmax=48, step=1):
+        for v in range(int(vmin), int(vmax), int(step)):
+            print(f"# input voltage {v} volts")
+            self.set_voltage(v)
+            callback(v)
